ca53fa97ddab46bd487948f214fd79b0dbcf41b4
Johan Strauss <576346@student.belgiumcampus.ac.za>
2021-02-25 17:03:04 +0200

    First Commit

---
+++ BusinessLogicLayer/LoginFactory.java
@@ -0,0 +1,23 @@
+package BusinessLogicLayer;
+
+public class LoginFactory{
+
+    Client client;
+
+    public Client GetEntry(String SignOrLog){
+
+        switch (SignOrLog) {
+            case "Log In":
+                
+                break;
+        
+            case "Sign Up":
+                
+                break;
+        
+            default:
+                break;
+        }
+
+    }
+}
---
+++ BusinessLogicLayer/PlanEventFactory.java
@@ -0,0 +1,25 @@
+package BusinessLogicLayer;
+
+public class PlanEventFactory {
+    Events events;
+
+    public Events PlanMenu(int choice){
+
+        switch (choice) {
+            case 1:
+                
+                break;
+        
+            case 2:
+                
+                break;
+        
+            case 3:
+                
+                break;
+        
+            default:
+                break;
+        }
+    }
+}
