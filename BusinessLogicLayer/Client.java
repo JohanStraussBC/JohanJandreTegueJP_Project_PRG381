c9530897ce4f59f239c2f167b47b59152a1c30d7
Johan Strauss <576346@student.belgiumcampus.ac.za>
2021-02-25 19:40:46 +0200

    Commit 6

--- BusinessLogicLayer/Client.java
+++ BusinessLogicLayer/Client.java
@@ -1,11 +1,22 @@
 package BusinessLogicLayer;
+import java.util.*;
 
 public class Client {
     
     void getClientDetails(){
-        
+
         String name;
         String surname;
         String cellNumber;
+
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter name:");
+        name = scn.nextLine();
+
+        System.out.println("Enter surname:");
+        surname = scn.nextLine();
+
+        System.out.println("Enter cell number:");
+        cellNumber = scn.nextLine();
     }
 }
